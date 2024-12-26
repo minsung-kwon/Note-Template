29abdcf01827febf7626ec30549dc51a255326ae
minsung-kwon <87001868+minsung-kwon@users.noreply.github.com>
2024-12-27 01:20:31 +0900

    Update UnionFind.cpp

--- code/DataStructure/UnionFind.cpp
+++ code/DataStructure/UnionFind.cpp
@@ -1,14 +1,14 @@
 void init(int n){
-  for(int i=1; i<=n; i++) par[i] = i, Rank[i] = 1;
+    for(int i=1; i<=n; i++) par[i] = i, Rank[i] = 1;
 }
 int find(int v){
-  if(v == par[v]) return v;
-  return par[v] = find(par[v]);
+    if(v == par[v]) return v;
+    return par[v] = find(par[v]);
 }
 bool merge(int u, int v){
-  u = find(u), v = find(v);
-  if(u == v) return true;
-  if(Rank[u] > Rank[v]) swap(u, v);
-  par[u] = v;
-  if(Rank[u] == Rank[v]) Rank[v]++;
+    u = find(u), v = find(v);
+    if(u == v) return true;
+    if(Rank[u] > Rank[v]) swap(u, v);
+    par[u] = v;
+    if(Rank[u] == Rank[v]) Rank[v]++;
 }
