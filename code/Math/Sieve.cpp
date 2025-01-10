367df224a850248183c802c7dfc34f787cde0b73
minsung-kwon <87001868+minsung-kwon@users.noreply.github.com>
2025-01-10 11:55:56 +0900

    Update Sieve.cpp

--- code/Math/Sieve.cpp
+++ code/Math/Sieve.cpp
@@ -1,6 +1,5 @@
 int SP[5050505]; // SP[i] = i의 가장 작은 소인수
 vector<int> Primes;
-
 // n 이하의 모든 소수를 구함
 void Sieve(int n){
   for(int i=2; i<=n; i++){
@@ -8,7 +7,6 @@ void Sieve(int n){
     for(int j=i; j<=n; j+=i) if(!SP[j]) SP[j] = i;
   }
 }
-
 // Sieve 먼저 호출해야 함
 vector<pair<int,int>> Factorize(int n){
   vector<pair<int,int>> res;
