cfaa189d0965ef29fdf191dce580f69d0e9de84a
minsung-kwon <87001868+minsung-kwon@users.noreply.github.com>
2025-01-10 11:57:57 +0900

    Update SegmentTreeLazy.cpp

--- code/DataStructure/SegmentTreeLazy.cpp
+++ code/DataStructure/SegmentTreeLazy.cpp
@@ -3,14 +3,12 @@
 // 52만 -> 1 << 19 (524288), 100만 -> 1 << 20 (1048576)
 constexpr int SZ = 1 << 20;
 ll T[SZ<<1], L[SZ<<1];
-
 void Push(int node, int s, int e){
     if(L[node] == 0) return;
     T[node] += (e - s + 1) * L[node];
     if(s != e) L[node*2] += L[node], L[node*2+1] += L[node];
     L[node] = 0;
 }
-
 // [l, r]번째 수에 v를 더함, 0 <= l <= r < SZ
 void RangeAdd(int l, int r, ll v, int node=1, int s=0, int e=SZ-1){
     Push(node, s, e);
@@ -21,7 +19,6 @@ void RangeAdd(int l, int r, ll v, int node=1, int s=0, int e=SZ-1){
     RangeAdd(l, r, v, node*2+1, m+1, e);
     T[node] = T[node*2] + T[node*2+1];
 }
-
 // [l, r]번째 수의 합을 구함
 ll RangeSum(int l, int r, int node=1, int s=0, int e=SZ-1){
     Push(node, s, e);
