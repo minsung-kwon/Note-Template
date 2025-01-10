1a8a362f0951fd141ac23ca5e0a890fd0c6501cd
minsung-kwon <87001868+minsung-kwon@users.noreply.github.com>
2025-01-10 11:57:47 +0900

    Update SegmentTree.cpp

--- code/DataStructure/SegmentTree.cpp
+++ code/DataStructure/SegmentTree.cpp
@@ -3,12 +3,10 @@
 // 52만 -> 1 << 19 (524288), 100만 -> 1 << 20 (1048576)
 constexpr int SZ = 1 << 20;
 ll T[SZ<<1];
-
 void Set(int x, ll v){ // x번째 수를 v로 지정, x는 0 이상 SZ 미만
     x += SZ; T[x] = v;
     while(x /= 2) T[x] = T[x*2] + T[x*2+1];
 }
-
 ll Sum(int l, int r){ // [l, r] 구간의 합
     ll res = 0;
     for(l+=SZ, r+=SZ; l<=r; l/=2, r/=2){
